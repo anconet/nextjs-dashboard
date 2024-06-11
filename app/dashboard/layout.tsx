61d789ddbc5683abded6aa5e7fe32c9e5a2a52e4
Anthony Anconetani <anthony.anconetani@gmail.com>
2024-06-11 07:52:45 -0500

    Fix a build issue with that silly <SideNav></SideNav> thing

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,8 +4,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <div className="bg-slate-600 flex h-screen flex-col md:flex-row md:overflow-hidden">
             <div className="w-full flex-none md:w-64">
-                <SideNav>
-                </SideNav>
+                <SideNav></SideNav>
             </div>
             <div className="flex-grow p-6 md:overflow-y-auto md:p-12>">{children}</div>
         </div >)
